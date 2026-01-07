9434fbc52acb8fd3aed4d6e7adef5ca6ff3d118d
Sergio <sgarciar@edu.tecnocampus.cat>
2026-01-07 13:07:18 +0100

    first commit

---
+++ EZTrade/src/main/java/com/trading/platform/eztrade/EzTradeApplication.java
@@ -0,0 +1,13 @@
+package com.trading.platform.eztrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzTradeApplication.class, args);
+    }
+
+}
---
+++ EZTrade/src/test/java/com/trading/platform/eztrade/EzTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading.platform.eztrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
